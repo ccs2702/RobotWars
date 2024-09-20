134e5c4437c4944ed26878e79c73ed900740593b
Schumacher <colin.schumacher@btc-ag.com>
2024-09-20 12:22:43 +0200

    20.09.

--- src/main/java/com/btcag/bootcamp/test.java
+++ src/main/java/com/btcag/bootcamp/test.java
@@ -9,34 +9,5 @@ import java.awt.event.ActionListener;
 public class test {
     public static void main(String[] args) {
 
-        Object [][] data = {
-                {"USA", 1 },
-                {"China", 2 },
-                {"Germany", 3 },
-        };
-
-        String [] collums = {"Skin", "Skinnummer"};
-        DefaultTableModel tableModel = new DefaultTableModel(data, collums);
-
-        JTable jtable = new JTable(tableModel);
-
-        JPanel jpanel = new JPanel();
-        jpanel.add(new JScrollPane(jtable));
-
-        JButton addButton = new JButton("Skin1");
-        JButton addButton1 = new JButton("Skin 2");
-        JButton addButton2 = new JButton("Skin 3");
-
-
-        jpanel.add(addButton);
-        jpanel.add(addButton1);
-        jpanel.add(addButton2);
-
-        JFrame frame = new JFrame("Skinauswahl");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(jpanel);
-
-        frame.pack();
-        frame.setVisible(true);
     }
 }
