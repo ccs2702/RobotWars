84e6101e65c4d353ef47bc299db6b2e9bd620256
Schumacher <colin.schumacher@btc-ag.com>
2024-09-23 07:22:53 +0200

    21.09.

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -3,28 +3,49 @@ package com.btcag.bootcamp;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Bitte geben Sie Ihren Namen ein:");
+        Scanner scanner = new Scanner(System.in);
+        String name1 = scanner.nextLine();
 
         System.out.println("Bitte geben Sie den Namen für Ihren Roboter ein:");
-        Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        System.out.println("Herzlich Willkommen zu RobotWars!" + "\n");
+
+        System.out.println("///////////////");
+        Thread.sleep(800);
+        System.out.println("Daten werden Geladen...");
+        Thread.sleep(800);
+        System.out.println("///////////////");
+        Thread.sleep(800);
+        System.out.println("Map wird initialisiert...");
+        Thread.sleep(800);
+        System.out.println("///////////////");
+        Thread.sleep(800);
+        System.out.println("Starten des Spiels...\n");
+        Thread.sleep(800);
+
+        System.out.println("Herzlich Willkommen zu RobotWars" + name1 + "\n");
         System.out.println("Ein Spiel in dem Roboter sich beweisen müssen.");
         System.out.println("Nur einer kann Gewinnen!" + "\n");
 
-        System.out.println("[R][O][B][O][T]                     [W][A][R][S]" + "\n");
+        Thread.sleep(1000);
 
-        System.out.println("[ ][A][B][C][D][E][F][G][H][I][J][K][L][M][N][O]");
-        System.out.println("[0][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
-        System.out.println("[1][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
-        System.out.println("[2][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
-        System.out.println("[3][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
-        System.out.println("[4][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
-        System.out.println("[5][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
-        System.out.println("[6][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
-        System.out.println("[7][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
-        System.out.println("[8][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
-        System.out.println("[9][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]" + "\n");
+        System.out.println("[R][O][B][O][T]         [W][A][R][S]" + "\n");
+
+        Thread.sleep(500);
+
+        System.out.println("[ ][A][B][C][D][E][F][G][H][I][J][K]");
+        System.out.println("[0][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
+        System.out.println("[1][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
+        System.out.println("[2][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
+        System.out.println("[3][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
+        System.out.println("[4][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
+        System.out.println("[5][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
+        System.out.println("[6][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
+        System.out.println("[7][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
+        System.out.println("[8][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]");
+        System.out.println("[9][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]" + "\n");
         System.out.println("Ihr Roboter heißt: " + name + "\n");
         System.out.println("Dein Roboter befindet sich auf dem Feld A:0");
     }
